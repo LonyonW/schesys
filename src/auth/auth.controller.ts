e43bfdb810c45784e1b0eefa0a665e94d80bf512
Nicolas Gutierrez <yefry-nicolas123@hotmail.com>
2025-04-13 11:33:21 -0500

    fix(auth): restrict user registration to admins only

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterAuthDto } from './dto/register-auth.dto';
 import { LoginAuthDto } from './dto/login-auth.dto';
+import { hasRoles } from './jwt/has-roles';
+import { JwtRole } from './jwt/jwt-role';
+import { JwtAuthGuard } from './jwt/jwt-auth.guard';
+import { JwtRolesGuard } from './jwt/jwt-roles.guard';
 
 @Controller('auth')
 export class AuthController {
 
     constructor(private authService: AuthService) {}
 
+
+    @hasRoles(JwtRole.ADMIN) // PTOTECCION DE RUTAS por rol
+    @UseGuards(JwtAuthGuard, JwtRolesGuard) // PTOTECCION DE RUTAS token obligado
     @Post('register') // http://localhost:3000/auth/register -> POST
     register(@Body() user: RegisterAuthDto) {
         return this.authService.register(user);
