6174bd4619c6c73251ab6b10b9bef853a336c048
Nicolas Gutierrez <yefry-nicolas123@hotmail.com>
2025-05-12 18:41:44 -0500

    fix(subjects) added more components

--- src/subjects/enums/subject-component.enum.ts
+++ src/subjects/enums/subject-component.enum.ts
@@ -3,5 +3,12 @@ export enum SubjectComponent {
     TECHNICAL = 'tecnica',
     THEORETICAL = 'teorica',
     MULTIDISCIPLINARY = 'multidisciplinaria',
+    PROJECTUAL = 'proyectual',
+    EXPRESSION = 'expresion',
+    HISTORY_THEORY = 'historia_teoria',
+    URBAN_ENVIRONMENTAL = 'urbano_ambiental',
+    ELECTIVE_DISCIPLINARY = 'electivo_disciplinar',
+    ELECTIVE_MULTIDISCIPLINARY = 'electivo_multidisciplinar',
+    INSTITUTIONAL = 'institucional',
   }
   
