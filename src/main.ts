534bcbba6229e98012f449d483b2dda97b079429
Nicolas Gutierrez <yefry-nicolas123@hotmail.com>
2025-05-15 20:49:06 -0500

    fix(security) cors enabled for all IPs

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://sage-frontend-3dek.onrender.com'], // PRODUCCION
-    //origin: true, // Para pruebas mientras el front no este desplegado
+    //origin: ['https://sage-frontend-3dek.onrender.com'], // PRODUCCION
+    origin: true, // Para pruebas mientras el front no este desplegado
     methods: 'GET,POST,PUT,PATCH,DELETE',
     credentials: true, // para los tokens
   });
